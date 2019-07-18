5312278227d841ec84a1f0befa47e172e89ccb3a
Ryan Carter <33025230+Ryan-rc-Carter@users.noreply.github.com>
2019-07-18 16:55:54 +0100

    Assignment started
    Added Users Superclass

---
+++ Apex Library application/src/Model/Users.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author rcarter4
+ */
+public abstract class Users {
+    
+    private int id;
+    private String password;
+    private Boolean isAdmin;
+    private String firstName;
+    private String lastName;
+    
+    public Users (int tempId, String tempPassword, String tempFirstName, String tempLastName){
+    
+        id = tempId;
+        password = tempPassword;
+        
+        firstName = tempFirstName;
+        lastName = tempLastName;
+    }   
+
+    public int getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+    
+    
+    
+    
+    
+}
